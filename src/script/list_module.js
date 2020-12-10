ef2489409179891e14214478f661a12cff38382e
Userwenten <1158084746@qq.com>
2020-12-10 10:44:56 +0800

    first commit

---
+++ src/script/list_module.js
@@ -0,0 +1,32 @@
+define(['jlazyload'], () => {
+    return {
+        init: function() {
+            const $list = $('.list ul');
+            alert(222)
+            $.ajax({
+                url: 'http://127.0.0.1/dashboard/JS2010/week07/Day%2034_sass%e5%92%8cmodule/projectname_1208/php/listdata.php',
+                dataType: 'json'
+            }).done(function(data) {
+                let $strhtml = '';
+                $.each(data, function(index, value) {
+                    $strhtml += `
+                        <li>
+                            <a href="detail.html?sid=${value.sid}">
+                                 <img class="lazy" data-original="${value.url}" width="200" height="200"/>
+                                <p>${value.title}</p>
+                                <span>￥${value.price}</span>
+                            </a>
+                        </li>
+                    `;
+                });
+                $list.html($strhtml);
+                // 渲染的下面进行懒加载操作
+                $(function() { //页面加载完成
+                    $("img.lazy").lazyload({
+                        effect: "fadeIn" //显示方法：谈入
+                    });
+                });
+            });
+        }
+    }
+});
---
+++ src/script/registry.js
@@ -0,0 +1,22 @@
+! function($) {
+    const $username = $('#username');
+    const $span = $('span');
+
+    $username.on('blur', function() {
+        $.ajax({
+            type: 'post',
+            url: 'http://127.0.0.1/dashboard/JS2010/week06/Day%2029-Day%2031_jquery/projectname/php/reg.php',
+            data: {
+                xingming: $username.val()
+            }
+        }).done(function(data) { //data就是后端返回的结果
+            console.log(data);
+            if (!data) { //不存在
+                $span.css('color', 'green').html('√');
+            } else { //存在
+                $span.css('color', 'red').html('该用户名已存在');
+            }
+        });
+    });
+
+}(jQuery);
